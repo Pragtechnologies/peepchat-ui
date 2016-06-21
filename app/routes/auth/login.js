c46054ba8e50c39afe5abae003d3e29d1bf1713b
Noel del Castillo <noel@pragtechnologies.com>
2016-06-21 16:52:48 +0800

    Applies authentication and proper route structure

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route, inject } = Ember;
+
+export default Route.extend({
+  session: inject.service(),
   actions: {
     doLogin() {
-      alert('login to');
+      const user = this.get('currentModel');
+      this.get('session')
+        .authenticate(
+          'authenticator:peepchat', user.email, user.password
+        );
     }
   },
   model() {
